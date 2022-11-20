5b23b52f41c617580fe1b8da1332d6f84a9e53be
ponapalt <ponapalt@shillest.net>
2022-11-20 17:48:06 +0900

    modification for old compiler

--- src/enc/backward_references_enc.c
+++ src/enc/backward_references_enc.c
@@ -154,7 +154,7 @@ static PixOrCopyBlock* BackwardRefsNewBlock(VP8LBackwardRefs* const refs) {
   if (b == NULL) {   // allocate new memory chunk
     const size_t total_size =
         sizeof(*b) + refs->block_size * sizeof(*b->start);
-    b = (PixOrCopyBlock*)WebPSafeMalloc(1ULL, total_size);
+    b = (PixOrCopyBlock*)WebPSafeMalloc(1Ui64, total_size);
     if (b == NULL) {
       refs->error |= 1;
       return NULL;
